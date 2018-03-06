70a60c4e416188e41c09143a4c65b52d4b633871
jeffansay <jeffansay21@gmail.com>
2018-03-06 21:50:40 +0800

    adding the route for register to redirect from login page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HttpModule } from '@angular/http';
 import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { AuthService } from './services/auth.service';
     HomeComponent,
     RegisterComponent,
     DashboardComponent,
+    LoginComponent,
 
   ],
   imports: [
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -19,7 +20,7 @@ export class RegisterComponent implements OnInit {
   userMessage;
 
 
-  constructor(private formBuilder: FormBuilder, private authService : AuthService) {
+  constructor(private formBuilder: FormBuilder, private authService : AuthService, private router : Router) {
         this.createForm();
 
    }
@@ -90,6 +91,9 @@ export class RegisterComponent implements OnInit {
        } else {
          this.messageClass = 'alert alert-success';
         this.message = data.message;
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        }, 2000)
        }
 
 
